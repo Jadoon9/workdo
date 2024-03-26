425676d210f7b4a682338801c88d34090e4fd9f0
Shahzeb Jadoon <shahzebjadoon0@gmaile.com>
2024-03-27 00:15:13 +0500

    fixed links disable

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -8,17 +8,17 @@ const navitems = [
   {
     id: 2,
     name: "Experiences",
-    link: "/shop-now",
+    link: "/",
   },
   {
     id: 3,
     name: "How it works",
-    link: "/minutes-details",
+    link: "/",
   },
   {
     id: 4,
     name: "Minutes Blog",
-    link: "/faq",
+    link: "/",
   },
 ];
 
